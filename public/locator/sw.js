5a7cd7951deec9758482a55ceaa28a66a5a74416
Michael FIG <mfig@agoric.com>
2022-09-23 10:43:53 -0600

    feat(locator): simplify and add better offline support

--- public/locator/sw.js
+++ public/locator/sw.js
@@ -1,9 +1,9 @@
-const filesToCache = ['./', 'index.html', '404.html', 'favicon-full.ico'];
-const staticCacheName = 'local-cache-v1';
+const filesToCache = ['./', 'index.html', 'favicon-full.png'];
+const staticCacheName = 'locator-cache-v2';
 self.addEventListener('install', event => {
   console.log('Installing service worker and adding files to cache');
   event.waitUntil(
-    caches.open(staticCacheName).then(cache => cache.addAll(filesToCache)),,
+    caches.open(staticCacheName).then(cache => cache.addAll(filesToCache)),
   );
 });
 
@@ -11,14 +11,14 @@ self.addEventListener('fetch', event => {
   // console.log('Fetching', event.request);
   event.respondWith(
     caches
-      .match(event.request)
+      .open(staticCacheName)
+      .then(cache => cache.match(event.request))
       .then(response => {
         if (response) {
           return response;
         }
         console.log('Network request for ', event.request.url);
         return fetch(event.request);
-      })
-      .catch(error => caches.match('/404.html')),
+      }),
   );
 });
