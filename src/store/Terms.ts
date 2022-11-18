b445404256cc9c755d36fdfab4602e93fdd1f90c
Turadg Aleahmad <turadg@agoric.com>
2022-11-18 10:49:14 -0800

    migrate store/Terms.ts

--- src/store/Terms.ts
+++ src/store/Terms.ts
@@ -4,7 +4,6 @@ import { TERMS_KEY, maybeSave, maybeLoad } from '../util/storage.js';
 // increment every time the terms change.
 export const LATEST_TERMS_INDEX = 0;
 
-export const agreeToTerms = (/** @type {number} */ index) =>
-  maybeSave(TERMS_KEY, index);
+export const agreeToTerms = (index: number) => maybeSave(TERMS_KEY, index);
 
 export const getTermsIndexAccepted = () => maybeLoad(TERMS_KEY) ?? -1;
