8c279b220205df4de99b630652a67d37d5774a52
Ikenna Omekam <iomekam@Ikennas-MacBook-Pro-2.local>
2023-06-28 15:12:12 -0700

    fix: use location.origin for local network config

---
+++ wallet/src/components/tests/ConnectionSettingsDIalong.test.tsx
@@ -0,0 +1,66 @@
+import { mount } from 'enzyme';
+import ConnectionSettingsDialog from '../ConnectionSettingsDialog';
+import { DEFAULT_CONNECTION_CONFIGS, KnownNetworkConfigUrls } from '../../util/connections'; 
+import { Input, Select, TextField, ThemeProvider, createTheme } from '@mui/material';
+import { act } from '@testing-library/react';
+
+jest.mock('lodash-es/isEqual', () => () => true);
+
+const appTheme = createTheme({
+  palette: {
+    // @ts-expect-error not in Palette
+    cancel: {
+      main: '#595959',
+    },
+  },
+});
+
+const MOCK_LOCALHOST = "http://foobar:440"
+
+const withApplicationContext =
+  (Component, _) =>
+  ({ ...props }) => {
+    return (
+      <ThemeProvider theme={appTheme}>
+        <Component {...props} />
+      </ThemeProvider>
+    );
+  };
+
+jest.mock('../../contexts/Application', () => {
+  return { withApplicationContext };
+});
+
+describe("Connection setting dialog", () => {
+  const { location } = window;
+
+  beforeAll(() => {
+    // @ts-expect-error mocking window.location
+    delete window.location;
+
+    // @ts-expect-error mocking window.location
+    window.location = { origin: MOCK_LOCALHOST };
+  });
+
+  afterAll(() => {
+    window.location = location;
+  });
+
+  test('displays the dapp', () => {
+  const component = mount(
+    <ConnectionSettingsDialog 
+      allConnectionConfigs={DEFAULT_CONNECTION_CONFIGS} 
+      connectionConfig={{ href: KnownNetworkConfigUrls.main }}
+      open
+      />);
+  
+  const networkSelect = component.find(Select).first();
+  act(() => networkSelect.props().onChange({ target: { value: 'localhost' } }));
+  component.update();
+  
+  const textField = component.find(TextField).first()
+  const inputField = textField.find("input").first()
+  
+  expect(inputField.prop("value")).toEqual(`${MOCK_LOCALHOST}/wallet/network-config`)
+  });
+});
