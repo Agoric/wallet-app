9b155e16cf87beb6d9c644e93340fe1b030762ef
rabi-siddique <rabisiddique27@gmail.com>
2024-08-28 16:02:22 +0500

    ci: resolve localhost with cypress e2e tests

--- test/e2e/specs/test.spec.js
+++ test/e2e/specs/test.spec.js
@@ -27,7 +27,7 @@ describe('Wallet App Test Cases', { execTimeout: DEFAULT_EXEC_TIMEOUT }, () => {
     });
 
     it('should setup web wallet successfully', () => {
-      cy.visit('http://localhost:3000/wallet/');
+      cy.visit('/wallet/');
 
       cy.get('input[type="checkbox"]').check();
       cy.contains('Proceed').click();
@@ -60,146 +60,146 @@ describe('Wallet App Test Cases', { execTimeout: DEFAULT_EXEC_TIMEOUT }, () => {
         .should('exist');
     });
 
-    // it('should succeed in provisioning a new wallet ', () => {
-    //   cy.skipWhen(AGORIC_NET === 'local');
-    //   cy.setupWallet({
-    //     createNewWallet: true,
-    //     walletName: 'newWallet',
-    //     selectedChains: ['Agoric'],
-    //   });
+    it('should succeed in provisioning a new wallet ', () => {
+      cy.skipWhen(AGORIC_NET === 'local');
+      cy.setupWallet({
+        createNewWallet: true,
+        walletName: 'newWallet',
+        selectedChains: ['Agoric'],
+      });
 
-    //   cy.getWalletAddress('Agoric').then((address) => {
-    //     // provision BLD
-    //     cy.provisionFromFaucet(address, 'delegate');
-    //     // provision IST
-    //     cy.provisionFromFaucet(address, 'client');
-    //   });
+      cy.getWalletAddress('Agoric').then((address) => {
+        // provision BLD
+        cy.provisionFromFaucet(address, 'delegate');
+        // provision IST
+        cy.provisionFromFaucet(address, 'client');
+      });
 
-    //   cy.visit('/wallet/');
-    //   cy.get('span').contains('ATOM').should('exist');
-    //   cy.get('span').contains('BLD').should('exist');
-    // });
+      cy.visit('/wallet/');
+      cy.get('span').contains('ATOM').should('exist');
+      cy.get('span').contains('BLD').should('exist');
+    });
 
-    // it('should switch to "My Wallet" successfully', () => {
-    //   cy.skipWhen(AGORIC_NET === 'local');
-    //   cy.switchWallet('My Wallet').then((taskCompleted) => {
-    //     expect(taskCompleted).to.be.true;
-    //   });
-    // });
-    // it('should add keys using agd from the CLI successfully', () => {
-    //   cy.addKeys({
-    //     keyName: userConfig.userKeyName,
-    //     mnemonic: userConfig.userWalletMnemonic,
-    //     expectedAddress: userConfig.userWalletAddress,
-    //   });
-    // });
+    it('should switch to "My Wallet" successfully', () => {
+      cy.skipWhen(AGORIC_NET === 'local');
+      cy.switchWallet('My Wallet').then((taskCompleted) => {
+        expect(taskCompleted).to.be.true;
+      });
+    });
+    it('should add keys using agd from the CLI successfully', () => {
+      cy.addKeys({
+        keyName: userConfig.userKeyName,
+        mnemonic: userConfig.userWalletMnemonic,
+        expectedAddress: userConfig.userWalletAddress,
+      });
+    });
 
-    // it('should create a vault minting 400 ISTs and giving 80 ATOMs as collateral', () => {
-    //   cy.skipWhen(AGORIC_NET !== 'local');
-    //   cy.createVault({
-    //     wantMinted: 400,
-    //     giveCollateral: 80,
-    //     userKey: userConfig.userKeyName,
-    //   });
-    // });
+    it('should create a vault minting 400 ISTs and giving 80 ATOMs as collateral', () => {
+      cy.skipWhen(AGORIC_NET !== 'local');
+      cy.createVault({
+        wantMinted: 400,
+        giveCollateral: 80,
+        userKey: userConfig.userKeyName,
+      });
+    });
 
-    // it(
-    //   'should place a bid by discount from the CLI successfully and verify IST balance',
-    //   {
-    //     taskTimeout: DEFAULT_TASK_TIMEOUT,
-    //   },
-    //   () => {
-    //     cy.addNewTokensFound();
-    //     cy.getTokenAmount('IST').then((initialTokenValue) => {
-    //       cy.placeBidByDiscount({
-    //         fromAddress: userConfig.userWalletAddress,
-    //         giveAmount: '2IST',
-    //         discount: 5,
-    //       }).then(() => {
-    //         cy.getTokenAmount('IST').then((tokenValue) => {
-    //           expect(tokenValue).to.lessThan(initialTokenValue);
-    //         });
-    //       });
-    //     });
-    //     cy.wait(5000);
-    //   },
-    // );
+    it(
+      'should place a bid by discount from the CLI successfully and verify IST balance',
+      {
+        taskTimeout: DEFAULT_TASK_TIMEOUT,
+      },
+      () => {
+        cy.addNewTokensFound();
+        cy.getTokenAmount('IST').then((initialTokenValue) => {
+          cy.placeBidByDiscount({
+            fromAddress: userConfig.userWalletAddress,
+            giveAmount: '2IST',
+            discount: 5,
+          }).then(() => {
+            cy.getTokenAmount('IST').then((tokenValue) => {
+              expect(tokenValue).to.lessThan(initialTokenValue);
+            });
+          });
+        });
+        cy.wait(5000);
+      },
+    );
 
-    // it('should view the bid from CLI', () => {
-    //   cy.listBids(userConfig.userWalletAddress);
-    // });
+    it('should view the bid from CLI', () => {
+      cy.listBids(userConfig.userWalletAddress);
+    });
 
-    // it('should see an offer placed in the previous test case', () => {
-    //   cy.visit('/wallet/');
+    it('should see an offer placed in the previous test case', () => {
+      cy.visit('/wallet/');
 
-    //   cy.contains('Offer').should('be.visible');
-    //   cy.contains('Give Bid').should('be.visible');
-    //   cy.contains('2.00 IST').should('be.visible');
-    //   cy.contains('from IST').should('be.visible');
-    //   cy.contains('Arguments').should('be.visible');
-    // });
+      cy.contains('Offer').should('be.visible');
+      cy.contains('Give Bid').should('be.visible');
+      cy.contains('2.00 IST').should('be.visible');
+      cy.contains('from IST').should('be.visible');
+      cy.contains('Arguments').should('be.visible');
+    });
 
-    // it('should cancel the bid by discount and verify IST balance', () => {
-    //   cy.getTokenAmount('IST').then((initialTokenValue) => {
-    //     cy.visit('/wallet/');
-    //     cy.contains('Exit').click();
-    //     cy.acceptAccess().then((taskCompleted) => {
-    //       expect(taskCompleted).to.be.true;
-    //     });
-    //     cy.contains('Accepted', { timeout: DEFAULT_TIMEOUT }).should('exist');
-    //     cy.getTokenAmount('IST').then((tokenValue) => {
-    //       expect(tokenValue).to.greaterThan(initialTokenValue);
-    //     });
-    //   });
-    // });
+    it('should cancel the bid by discount and verify IST balance', () => {
+      cy.getTokenAmount('IST').then((initialTokenValue) => {
+        cy.visit('/wallet/');
+        cy.contains('Exit').click();
+        cy.acceptAccess().then((taskCompleted) => {
+          expect(taskCompleted).to.be.true;
+        });
+        cy.contains('Accepted', { timeout: DEFAULT_TIMEOUT }).should('exist');
+        cy.getTokenAmount('IST').then((tokenValue) => {
+          expect(tokenValue).to.greaterThan(initialTokenValue);
+        });
+      });
+    });
 
-    // it(
-    //   'should place a bid by price from the CLI successfully and verify IST balance',
-    //   {
-    //     taskTimeout: DEFAULT_TASK_TIMEOUT,
-    //   },
-    //   () => {
-    //     cy.getTokenAmount('IST').then((initialTokenValue) => {
-    //       cy.placeBidByPrice({
-    //         fromAddress: userConfig.userWalletAddress,
-    //         giveAmount: '1IST',
-    //         price: 8.55,
-    //       }).then(() => {
-    //         cy.getTokenAmount('IST').then((tokenValue) => {
-    //           expect(tokenValue).to.lessThan(initialTokenValue);
-    //         });
-    //       });
-    //     });
-    //   },
-    // );
+    it(
+      'should place a bid by price from the CLI successfully and verify IST balance',
+      {
+        taskTimeout: DEFAULT_TASK_TIMEOUT,
+      },
+      () => {
+        cy.getTokenAmount('IST').then((initialTokenValue) => {
+          cy.placeBidByPrice({
+            fromAddress: userConfig.userWalletAddress,
+            giveAmount: '1IST',
+            price: 8.55,
+          }).then(() => {
+            cy.getTokenAmount('IST').then((tokenValue) => {
+              expect(tokenValue).to.lessThan(initialTokenValue);
+            });
+          });
+        });
+      },
+    );
 
-    // it('should see an offer placed in the previous test case', () => {
-    //   cy.visit('/wallet/');
-    //   cy.contains('Offer').should('be.visible');
-    //   cy.contains('Give Bid').should('be.visible');
-    //   cy.contains('1.00 IST').should('be.visible');
-    //   cy.contains('from IST').should('be.visible');
-    //   cy.contains('Arguments').should('be.visible');
-    // });
+    it('should see an offer placed in the previous test case', () => {
+      cy.visit('/wallet/');
+      cy.contains('Offer').should('be.visible');
+      cy.contains('Give Bid').should('be.visible');
+      cy.contains('1.00 IST').should('be.visible');
+      cy.contains('from IST').should('be.visible');
+      cy.contains('Arguments').should('be.visible');
+    });
 
-    // it('should cancel the bid by price and verify IST balance', () => {
-    //   cy.getTokenAmount('IST').then((initialTokenValue) => {
-    //     cy.visit('/wallet/');
-    //     cy.contains('Exit').click();
-    //     cy.acceptAccess().then((taskCompleted) => {
-    //       expect(taskCompleted).to.be.true;
-    //     });
-    //     cy.contains('Accepted', { timeout: DEFAULT_TIMEOUT }).should('exist');
-    //     cy.getTokenAmount('IST').then((tokenValue) => {
-    //       expect(tokenValue).to.greaterThan(initialTokenValue);
-    //     });
-    //   });
-    // });
+    it('should cancel the bid by price and verify IST balance', () => {
+      cy.getTokenAmount('IST').then((initialTokenValue) => {
+        cy.visit('/wallet/');
+        cy.contains('Exit').click();
+        cy.acceptAccess().then((taskCompleted) => {
+          expect(taskCompleted).to.be.true;
+        });
+        cy.contains('Accepted', { timeout: DEFAULT_TIMEOUT }).should('exist');
+        cy.getTokenAmount('IST').then((tokenValue) => {
+          expect(tokenValue).to.greaterThan(initialTokenValue);
+        });
+      });
+    });
 
-    // it('should check the auction status and ensure required fields are present', () => {
-    //   cy.checkAuctionStatus().then((success) => {
-    //     expect(success).to.be.true;
-    //   });
-    // });
+    it('should check the auction status and ensure required fields are present', () => {
+      cy.checkAuctionStatus().then((success) => {
+        expect(success).to.be.true;
+      });
+    });
   });
 });
